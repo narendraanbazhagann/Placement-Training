cc922ccecd1fec1dd1796f5ce6b4b199268767a3
Narendra Anbazhagan <131426493+narendraanbazhagann@users.noreply.github.com>
2024-07-27 18:32:32 +0530

    Create pattern1.c

---
+++ 27.07.2024/pattern1.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void printDiamond(int n) {
+    for (int i = 1; i <= n; i++) {
+        for (int j = i; j < n; j++) {
+            printf(" ");
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = n - 1; i >= 1; i--) {
+        for (int j = n; j > i; j--) {
+            printf(" ");
+        }
+        for (int j = 1; j <= (2 * i - 1); j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of rows for the diamond: ");
+    scanf("%d", &n);
+    printDiamond(n);
+    return 0;
+}
