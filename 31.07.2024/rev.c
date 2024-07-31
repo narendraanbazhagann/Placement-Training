c3cb0a8b0f3e4641cb91adda059d1cd7a0f1b479
Narendra Anbazhagan <131426493+narendraanbazhagann@users.noreply.github.com>
2024-07-31 21:39:38 +0530

    Create rev.c

---
+++ 31.07.2024/rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the node structure
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end
+void insertAtEnd
